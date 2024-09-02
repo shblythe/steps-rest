860af6e2b63404d46ac2b1cddc8e5d286b9ff213
Stephen Blythe <stephen@urwick.co.uk>
2024-09-02 07:33:33 +0100

    Only update steps if increased

--- routes/rest.js
+++ routes/rest.js
@@ -3,13 +3,29 @@ const db =require('../database/appDb');
 
 var router = express.Router();
 
-router.post('/:player/:date/:steps', (req, res) => {
+router.post('/:player/:date/:steps', async (req, res) => {
     const player=req.params.player;
     const date=req.params.date;
     const steps=req.params.steps;
-    db.setup().then(()=>{
-        db.runQuery(`CALL AddSteps('${player}','${date}',${steps})`).then(()=>res.send())
-        .catch((e)=>res.status(404).send(e.message));
+    await db.setup().then(async ()=>{
+        const last = await db.getSingleObjectFromQueryRow(
+            `CALL GetPeriodSteps('${player}','${date}','${date}')`,
+            ["steps"]
+        );
+        const last_steps = last.steps*1;
+        console.log("*", steps, last_steps);
+        if (steps > last_steps) {
+            console.log("sent");
+            db.runQuery(`CALL AddSteps('${player}','${date}',${steps})`)
+                .then(() => {
+                    res.send();
+                })
+                .catch((e) => {
+                    res.status(404).send(e.message)
+                });
+        } else {
+            res.send();
+        }
     });
 });
 
